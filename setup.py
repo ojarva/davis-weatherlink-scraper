146f962c1e71cca784d81299fc3d71e208d275ef
Olli Jarva <olli.jarva@futurice.com>
2016-03-26 19:20:35 +0200

    0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='davis-weatherlink-scraper',
-    version='0.0.1',
+    version='0.0.2',
     description='Scraper and parser for Davis Weatherlink data',
     long_description=long_description,
     url='https://github.com/ojarva/davis-weatherlink-scraper',
